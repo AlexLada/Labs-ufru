2916bd03e2f1925f95cddd419e4ada2852a27a5b
AlexLada <vizuvi2@mail.ru>
2023-10-05 19:06:00 +0500

    добавил ссылку на гитхаб в ридми

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+// import Router from 'react-router-dom'
+// import Switch from 'react-router-dom'
+// import Route from 'react-router-dom'
+// import chess from "./Chess";
+
+export default function Header() {
+return(
+  <header className="bg-gradient-to-r from-[#45036f] to-[#ff5900]">
+    <Navbar/>
+  </header>
+  
+);
+}
